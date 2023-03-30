2ed33aa3b85c50f837e8a39a226b541650704b0b
ElmiraCuneyidova <ecuneyidova@gmail.com>
2023-03-30 00:52:58 -0700

    Add project files.

---
+++ task2/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Diagnostics.Metrics;
+using System.Dynamic;
+using System.Reflection.Metadata;
+
+namespace TASK2
+{
+    internal class Proqram
+    {
+        //static void Main(string[] args)
+        //{
+        // 1.Parametr kimi iki tam ədəd götürən və onların cəmini qaytaran metod
+        //yaradın.
+
+        //    Console.WriteLine(" Enter the first number: ");
+        //    int fisrt = int.Parse(Console.ReadLine());
+        //    Console.WriteLine(" Enter the second number: ");
+        //    int second = int.Parse(Console.ReadLine());
+
+        //    MyMethod(fisrt, second);
+
+
+        //}
+        //static void MyMethod(int first, int second)
+        //{
+        //    int sum = first + second;
+        //    Console.WriteLine("sum is " + sum);
+        //}
+
+
+
+
+        //static void Main(string[] args)
+        //{
+
+        //3.Parametr kimi float qəbul edən və onu ən yaxın tam ədədə
+        //  yuvarlaqlaşdıran metod yazın.
+
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine("Enter your number: ");
+        //    float num3 = float.Parse(Console.ReadLine());
+        //    float num3_ = (float)Math.Round(num3);
+
+        //  Round (num3_ , num3);
+        //}
+        //static void Round(float num3_ , float num3)
+        //{
+
+        //    Console.WriteLine(num3_) ;
+        //}
+
+
+
+        // 4.Tam ədədi parametr kimi qəbul edən və ədədin cüt və ya tək olduğunu
+        //göstərən və geriyə boolean qaytaran metod yazın.
+
+
+        //static void Main(string[] args)
+        //{
+
+
+        //    Console.WriteLine("Enter a number: ");
+        //    int num4 = int.Parse(Console.ReadLine());
+
+        //    Task4(num4);
+        //}
+        //static void Task4(int num4)
+        //{
+        //    if (num4 % 2 == 0)
+        //    {
+        //        Console.WriteLine(num4 + " is even");
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine(num4 + " is odd");
+        //    }
+        //}
+
+
+        //5.Parametr kimi boolean götürən və string qaytaran metod yaradın.
+        //Boolean doğrudursa, metod "Bəli", yanlışdırsa, "Xeyr" qaytarmalıdır.
+
+
+
+        //6.İki string parametr kimi götürən və onları birləşdirən metod yazın.Bir
+        //oyunçunun ad və soyadlarından tam adını yaratmaq üçün bu metoddan
+        //istifadə edin.
+
+
+
+
+        //7.Parametr kimi decimal qəbul edən və ədədin kvadratını qaytaran metod
+        //yaradın.
+
+
+
+
+        //8.Parametr kimi int qəbul edən və Sade olub olmadıgına görə bool deyeri
+        //qaytarır.Main methodda bool true qaytararsa Console -Edet Sadedir
+        //yazısı cıxsın.
+
+
+
+
+        //9.Parametr olaraq 2 dənə int qəbul edən method yaradın(int number, int
+        //power).Bu method number ededinin power quvettine qaldirib,
+        //qaytarmalıdır.
+
+
+
+
+
+
+
+    }
+}
+
+
+
